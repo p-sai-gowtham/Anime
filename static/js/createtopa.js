bfc5de067a994468f96815c969e7ad92493a47bd
p-sai-gowtham <puvvulasaigowtham@gmail.com>
2023-10-31 13:54:53 +0530

    updated top-airing

--- static/js/createtopa.js
+++ static/js/createtopa.js
@@ -1,6 +1,7 @@
 export default async function createTopA(topAiringContent, id) {
     const url = `https://api.consumet.org/anime/gogoanime/info/${id}`;
     const result = await getData(url);
+    console.log(result)
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row', 'g-0', 'text-bg-dark');
     const colDiv1 = document.createElement('div');
@@ -16,17 +17,33 @@ export default async function createTopA(topAiringContent, id) {
     const cardTitle = document.createElement('h5');
     cardTitle.classList.add('card-title');
     cardTitle.textContent = result.title;
-    const cardText = document.createElement('p');
-    cardText.classList.add('card-text');
-    cardText.textContent = result.description;
+
+    const innerColDiv2 = document.createElement('div');
+    innerColDiv2.classList.add('card-text');
+    const otherName = document.createElement('h3');
+    otherName.classList.add('other-name');
+    otherName.textContent = result.otherName;
+    otherName.textContent = 'Synonyms: ' + otherName.textContent;
+    const genres = document.createElement('h3');
+    genres.classList.add('genres');
+    genres.textContent = result.genres.join(', ');
+    genres.textContent = 'Genres: ' + genres.textContent;
+    const details = document.createElement('p');
+    details.classList.add('details');
+    details.innerHTML = `
+    ${result.releaseDate} ${result.status} ${result.subOrDub}
+    `;
+
     cardBody.appendChild(cardTitle);
-    cardBody.appendChild(cardText);
+    innerColDiv2.appendChild(otherName);
+    innerColDiv2.appendChild(genres);
+    innerColDiv2.appendChild(details);
+    cardBody.appendChild(innerColDiv2);
     colDiv2.appendChild(cardBody);
     rowDiv.appendChild(colDiv1);
     rowDiv.appendChild(colDiv2);
-    topAiringContent.childNodes[1].remove();
-    topAiringContent.appendChild(rowDiv);
-
+    await topAiringContent.childNodes[1].remove();
+    await topAiringContent.appendChild(rowDiv);
 }
 
 
